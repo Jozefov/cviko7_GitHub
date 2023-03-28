b17dff62a25604d5438a1c7d692fba67323d5c9c
Jozefov <mn.mastaf@gmail.com>
2023-03-28 09:00:24 +0200

    druhy

--- buggy_python_code.py
+++ buggy_python_code.py
@@ -52,16 +52,16 @@ if __name__ == '__main__':
     print("3. Yaml deserialization vulnerability: use string=file.yaml")
     print("4. Use of assert statements vulnerability: run program with -O argument")
     choice  = input("Select vulnerability: ")
-    if choice == "1": 
+    if choice =="1": 
         new_person = Person("Vickie")  
         print_nametag(input("Please format your nametag: "), new_person)
-    elif choice == "2":
+    elif choice =="2":
         urlib_version = input("Choose version of urllib: ")
         fetch_website(urlib_version, url="https://www.google.com")
-    elif choice == "3":
+    elif choice =="3":
         load_yaml(input("File name: "))
         print("Executed -ls on current folder")
-    elif choice == "4":
-        password = input("Enter master password: ")
+    elif choice =="4":
+	password = input("Enter master password: ")
         authenticate(password)
 
